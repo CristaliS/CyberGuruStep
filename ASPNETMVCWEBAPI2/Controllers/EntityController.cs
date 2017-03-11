154e1beccad4e836ff1b5dd841b60f0abe9303da
CristaliS <cristalis16@gmail.com>
2017-03-11 10:03:09 +0200

    Added new controller

---
+++ ASPNETMVCWEBAPI2/Controllers/EntityController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNETMVCWEBAPI2.Controllers
+{
+    public class EntityController : ApiController
+    {
+        // GET: api/Entity
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Entity/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Entity
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Entity/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Entity/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
